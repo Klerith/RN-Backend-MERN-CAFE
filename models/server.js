af7596ae6c4084b40aa468a446fdac83ae5cba38
Fernando Herrera <fernando.herrera85@gmail.com>
2021-03-25 10:48:28 -0400

    Backend para el curso de RN

---
+++ models/server.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
+
+const { dbConnection } = require('../database/config');
+
+class Server {
+
+    constructor() {
+        this.app  = express();
+        this.port = process.env.PORT;
+
+        this.paths = {
+            auth:       '/api/auth',
+            buscar:     '/api/buscar',
+            categorias: '/api/categorias',
+            productos:  '/api/productos',
+            usuarios:   '/api/usuarios',
+            uploads:    '/api/uploads',
+        }
+
+
+        // Conectar a base de datos
+        this.conectarDB();
+
+        // Middlewares
+        this.middlewares();
+
+        // Rutas de mi aplicación
+        this.routes();
+    }
+
+    async conectarDB() {
+        await dbConnection();
+    }
+
+
+    middlewares() {
+
+        // CORS
+        this.app.use( cors() );
+
+        // Lectura y parseo del body
+        this.app.use( express.json() );
+
+        // Directorio Público
+        this.app.use( express.static('public') );
+
+        // Fileupload - Carga de archivos
+        this.app.use( fileUpload({
+            useTempFiles : true,
+            tempFileDir : '/tmp/',
+            createParentPath: true
+        }));
+
+    }
+
+    routes() {
+        
+        this.app.use( this.paths.auth, require('../routes/auth'));
+        this.app.use( this.paths.buscar, require('../routes/buscar'));
+        this.app.use( this.paths.categorias, require('../routes/categorias'));
+        this.app.use( this.paths.productos, require('../routes/productos'));
+        this.app.use( this.paths.usuarios, require('../routes/usuarios'));
+        this.app.use( this.paths.uploads, require('../routes/uploads'));
+        
+    }
+
+    listen() {
+        this.app.listen( this.port, () => {
+            console.log('Servidor corriendo en puerto', this.port );
+        });
+    }
+
+}
+
+
+
+
+module.exports = Server;
---
+++ routes/auth.js
@@ -0,0 +1,31 @@
+const { Router } = require('express');
+const { check } = require('express-validator');
+
+
+const { validarCampos, validarJWT } = require('../middlewares');
+
+
+const { login, googleSignin, validarTokenUsuario } = require('../controllers/auth');
+
+
+const router = Router();
+
+router.post('/login',[
+    check('correo', 'El correo es obligatorio').isEmail(),
+    check('password', 'La contraseña es obligatoria').not().isEmpty(),
+    validarCampos
+],login );
+
+router.post('/google',[
+    check('id_token', 'El id_token es necesario').not().isEmpty(),
+    validarCampos
+], googleSignin );
+
+
+router.get('/',[
+    validarJWT
+], validarTokenUsuario );
+
+
+
+module.exports = router;
